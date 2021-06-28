697428d34e8836c5cc1417faf42f529c09605626
beltranbot <beltranbot@gmail.com>
2021-06-28 10:30:30 -0500

    added users routes, user controller and signup view, dried up shared._errors view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :articles
   get "about", to: "about#index"
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 end
