a3849e79660c82e6ce897807cb1f277babb19a2f
beltranbot <beltranbot@gmail.com>
2021-06-29 22:09:20 -0500

    added admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to root_path
   end
@@ -56,8 +56,8 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if @user != current_user
-      flash[:alert] = "You can only edit your own account"
+    if @user != current_user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own account"
       redirect_to @user
     end
   end
