efa6780fccadb0fbb0e2878a43eea2412a6f5eab
beltranbot <beltranbot@gmail.com>
2021-06-30 21:20:46 -0500

    added create category functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,8 +3,25 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def index
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "category was successfully created"
+      redirect_to @category
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
